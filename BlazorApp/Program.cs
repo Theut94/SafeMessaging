7e8bf858f45832bba26733dfbd556f39185b6136
Theut94 <89963626+Theut94@users.noreply.github.com>
2024-12-11 21:02:46 +0100

    Convert to webassembly.

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -8,9 +8,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var connectionstring = builder.Configuration.GetConnectionString("PGConnectionString") ?? throw new NullReferenceException("Connectionstring not found.");
+//var connectionstring = builder.Configuration.GetConnectionString("PGConnectionString") ?? throw new NullReferenceException("Connectionstring not found.");
+var connectionstring = "Host=localhost;Port=5432;Username=postgres;Password=mysecretpassword;Database=postgres;";
 
 //Dependencies
+builder.Services.AddScoped<WebApp.FrontEndEncryption.IDiffieHellmanUtil, WebApp.FrontEndEncryption.DiffieHellmanUtil>();
 builder.Services.AddScoped<WebApp.FrontEndEncryption.IEncryptionUtil, WebApp.FrontEndEncryption.EncryptionUtil>();
 DataDependencies.AddDependencies(builder.Services, connectionstring);
 ApplicationDependencies.AddDependencies(builder.Services);
--- WebApp/FrontEndEncryption/IDiffieHellmanUtil.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApp.FrontEndEncryption
-{
-    public interface IDiffieHellmanUtil
-    {
-        Dictionary<ECDHKey,byte[]> GetKeys();
-        byte[] GetCommonSecret(byte[] OtherPublicKey, byte[] MyPrivateKey);
-
-    }
-}
